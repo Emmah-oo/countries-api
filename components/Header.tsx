9e4db424b560107671456a87448f97963172f29d
Emmah-oo <emmaochonogor715@gmail.com>
2024-03-13 15:42:20 +0100

    building UI

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between px-2 py-6 items-center shadow-md">
+      <h1 className="font-bold">Where in the world?</h1>
+
+      <div>
+        <h2 className="text-md">Dark Mode</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ lib/data.ts
@@ -0,0 +1,5 @@
+export const getCountries = async () => {
+  const data = await fetch(`https://restcountries.com/v3.1/all`);
+
+  return data.json;
+};
